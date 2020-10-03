ba17f189dac07afe0cbd3ff8add1fbec26adafab
Scott Crossen <scottcrossen42@gmail.com>
2020-10-02 23:26:51 -0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+SCRIPT_DIR="${BASH_SOURCE%/*}"
+
 git config --global user.name "Scott Crossen"
 git config --global user.email "scottcrossen42@gmail.com"
+
+cp -r "$SCRIPT_DIR"/home ~
+chmod +x ~/.bashrc.d
+chmod +x ~/.bashrc
