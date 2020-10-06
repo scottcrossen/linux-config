cf61e0f3060ced0910baee7486a19f66a56fb710
Scott Crossen <scottcrossen42@gmail.com>
2020-10-06 12:53:54 -0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -2,9 +2,16 @@
 
 SCRIPT_DIR="${BASH_SOURCE%/*}"
 
+# Install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+# Install git
+apt install git
 git config --global user.name "Scott Crossen"
 git config --global user.email "scottcrossen42@gmail.com"
 
+# Install bashrc
 cp -r "$SCRIPT_DIR"/home ~
 chmod +x ~/.bashrc.d
 chmod +x ~/.bashrc
