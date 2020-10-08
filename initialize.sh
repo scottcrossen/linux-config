7a375fa03d7d1fe2efef092e5742fc0e9829c18a
Scott Crossen <scottcrossen42@gmail.com>
2020-10-07 17:11:58 -0700

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -14,8 +14,8 @@ sudo chmod +x ~/.bashrc.d
 sudo chmod +x ~/.bashrc
 
 echo "Installing basic packages"
-sudo apt-get update > /dev/null
-sudo apt-get update > /dev/null && sudo apt-get install -y \
+sudo apt-get -qq update > /dev/null
+sudo apt-get -qq update > /dev/null && sudo apt-get -qq install -y \
     apt-transport-https \
     gnupg2 \
     curl \
@@ -32,10 +32,10 @@ sudo apt-get update > /dev/null && sudo apt-get install -y \
 
 echo "Installing Google Chrome"
 curl -sSLo google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt-get install -y ./google-chrome-stable_current_amd64.deb > /dev/null
+sudo apt-get -qq install -y ./google-chrome-stable_current_amd64.deb > /dev/null
 
 echo "Installing Git"
-sudo apt-get install git > /dev/null
+sudo apt-get -qq install git > /dev/null
 git config --global user.name "Scott Crossen"
 git config --global user.email "scottcrossen42@gmail.com"
 ssh-keygen -f ~/.ssh/id_rsa -q -N ""
@@ -48,14 +48,14 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update > /dev/null && sudo apt-get install -y docker-ce docker-ce-cli containerd.io > /dev/null
-usermod -aG docker $USER && newgrp docker
+sudo apt-get -qq update > /dev/null && sudo apt-get -qq install -y docker-ce docker-ce-cli containerd.io > /dev/null
+sudo usermod -aG docker $USER && newgrp docker
 echo "TODO: Remeber to execute `docker login`"
 
 echo "Installing Kubernetes"
 curl -sSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - > /dev/null &> /dev/null
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update > /dev/null && sudo apt-get install -y kubectl > /dev/null
+sudo apt-get -qq update > /dev/null && sudo apt-get -qq install -y kubectl > /dev/null
 
 echo "Installing Minikube"
 adduser $USER libvirt
@@ -67,13 +67,13 @@ cp "$SCRIPT_DIR"/systemd/minikube.service /lib/systemd/system/minikube.service
 systemctl daemon-reload
 
 echo "Installing I3"
-sudo apt-get install i3
+sudo apt-get -qq install i3
 sudo update-alternatives --install /usr/bin/x-session-manager x-session-manager /usr/bin/i3 60
 
 echo "Installing Visual Studio Code"
 curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add - > /dev/null &> /dev/null
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-sudo apt-get update > /dev/null && sudo apt-get install -y code > /dev/null
+sudo apt-get -qq update > /dev/null && sudo apt-get -qq install -y code > /dev/null
 
 echo "Installing Chrome Remote Desktop"
 curl -sSLo chrome-remote-desktop_current_amd64.deb https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
