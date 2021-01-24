76d8a32962ed9c8494c9149f294f32c70bb78690
Scott Crossen <scottcrossen42@gmail.com>
2021-01-24 08:08:57 +0000

    Added readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Linux Config
+
+My personal linux config and dotfiles
+
+### Usage
+
+```
+sudo apt-get install -y -qq git; git clone -q https://github.com/scottcrossen/linux-config.git && linux-config/initialize.sh scottcrossen "Scott Crossen" scottcrossen42@gmail.com
+```
+
--- initialize.sh
+++ initialize.sh
@@ -227,5 +227,6 @@ fi
 echo "Installing Ruby"
 gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -sSL https://get.rvm.io  | sed 's/rvm_install "$@"/rvm_install "$@" > \/dev\/null 2> \/dev\/null/g' | bash -s stable --ruby > /dev/null
+source /usr/local/rvm/scripts/rvm
 rvm rvmrc warning ignore allGemfiles
 
